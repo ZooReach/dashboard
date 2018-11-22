8065c5c96c4790131b052df06eab00137f3a3e22
Sakthivel Viswanathan <sakthivel.v@thoughtworks.com>
2018-11-22 10:50:18 +0530

    [Sakthivel] - UI Templates initial commit

--- app/app.py
+++ app/app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
+import json
+ 
 app = Flask(__name__)
-
-@app.route("/")
-@app.route("/index")
-def hello():
-    return render_template('extend.html', name="zooreach")
-
-if __name__=='__main__':
-	app.run()
+ 
+@app.route('/')
+@app.route('/home')
+def home():
+    with app.open_resource('data/nature.json') as f:
+        species = json.load(f)
+    return render_template('home.html', cards=species)
+        
+if __name__ == '__main__':
+    app.run()

494e9ec3c7d6cdbe3218ec5f33ddb0279a1850a7
Sakthivel Viswanathan <sakthivel.v@thoughtworks.com>
2018-11-22 15:49:19 +0530

    [Sakthivel] Update card disable option

--- app/app.py
+++ app/app.py
@@ -1,8 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 import json
+import os
  
 app = Flask(__name__)
  
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                     endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
+
 @app.route('/')
 @app.route('/home')
 def home():

449ad2efc77cc2958c564bf7445c4086c131a854
Abilash Rajasekaran <abilashr@IN-CBE-Abilash.local>
2018-11-22 07:14:39 +0530

    test templating

--- app/app.py
+++ app/app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/index")
 def hello():
-    return "Hello World this works!"
+    return render_template('extend.html', name="zooreach")
 
 if __name__=='__main__':
 	app.run()

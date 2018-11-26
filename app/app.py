552c1a95c671d4418beae46f617ff766ef20ecab
Abilash Rajasekaran <abilashr@IN-CBE-Abilash.local>
2018-11-26 15:17:45 +0530

    Initial setup to detail page

--- app/app.py
+++ app/app.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template, url_for
 import json
 import os
- 
+
 app = Flask(__name__)
- 
+
+
 @app.context_processor
 def override_url_for():
     return dict(url_for=dated_url_for)
+
 
 def dated_url_for(endpoint, **values):
     if endpoint == 'static':
@@ -17,6 +19,7 @@ def dated_url_for(endpoint, **values):
             values['q'] = int(os.stat(file_path).st_mtime)
     return url_for(endpoint, **values)
 
+
 @app.route('/')
 @app.route('/main')
 def home():
@@ -24,9 +27,11 @@ def home():
         species = json.load(f)
     return render_template('home/home.html', cards=species)
 
+
 @app.route('/category/<category>')
 def category(category):
-    return 'Page %s' % category
-        
+    return render_template('category/category.html', category=category)
+
+
 if __name__ == '__main__':
     app.run()

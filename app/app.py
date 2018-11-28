6c8cb9bd029f01482ff63abb45cb2cec491414f0
Abilash Rajasekaran <abilashr@IN-CBE-Abilash.local>
2018-11-28 18:34:59 +0530

    Add visualization template to detail page

--- app/app.py
+++ app/app.py
@@ -31,15 +31,32 @@ def home():
 @app.route('/category/<path:filename>')
 def category(filename):
     path = filename.split('/')
-    jsonFile = 'data/' + path[0] + '.json'
-    with app.open_resource(jsonFile) as f:
-        cards = json.load(f)
 
-    jsonData = cards
+    with app.open_resource(getJsonFile(path[0])) as file:
+        type = json.load(file)
+
+    return render_template('category/category.html', jsonData=getType(path, type), fullpath=path,
+                           jsfiles=getVisualFiles(filename))
+
+
+def getJsonFile(path):
+    return ('data/' + path + '.json')
+
+
+def getType(path, type):
+    jsonData = type
     for name in path:
         jsonData = jsonData['type'][name]
+    return jsonData
 
-    return render_template('category/category.html', jsonData=jsonData, fullpath=path)
+
+def getVisualFiles(filename):
+    directory = 'static/js/visualization/' + filename
+    jsfiles = []
+    if os.path.isdir(directory):
+        for files in os.listdir(directory):
+            jsfiles.append(filename + '/' + files)
+    return jsfiles
 
 
 if __name__ == '__main__':

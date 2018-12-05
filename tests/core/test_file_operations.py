fc3f8f2ebb61b1c40923ac397aef60c01e29d6fd
Sreekesh S <in-sreekesh@In-Cbe-Sreekesh.local>
2018-12-05 17:45:39 +0530

    test changes

--- tests/core/test_file_operations.py
+++ tests/core/test_file_operations.py
@@ -5,8 +5,8 @@ from app.core import file_operations as file
 class FileOperationTestCase(unittest.TestCase):
 
     def test_get_json_file(self):
-        self.assertEqual(file.get_json_file('path'), 'data/path.json')
-        self.assertEqual(file.get_json_file('append'), 'data/append.json')
+        self.assertEqual(file.get_json_file_path_from_data('path'), 'data/path.json')
+        self.assertEqual(file.get_json_file_path_from_data('append'), 'data/append.json')
 
     def test_append_files(self):
         self.assertEqual(file.append_files([], "filename", "files"), ['filename/files'])

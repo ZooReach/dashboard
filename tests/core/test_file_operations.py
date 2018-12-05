da91ca2cef108101948ca4acff06d731112c43ab
Abilash Rajasekaran <abilashr@IN-CBE-Abilash.local>
2018-12-05 16:59:56 +0530

    add tests for file operations

--- tests/core/test_file_operations.py
+++ tests/core/test_file_operations.py
@@ -1,7 +1,13 @@
 import unittest
 from app.core import file_operations as file
 
-class AppTestCase(unittest.TestCase):
 
-    def test_client(self):
+class FileOperationTestCase(unittest.TestCase):
+
+    def test_get_json_file(self):
         self.assertEqual(file.get_json_file('path'), 'data/path.json')
+        self.assertEqual(file.get_json_file('append'), 'data/append.json')
+
+    def test_append_files(self):
+        self.assertEqual(file.append_files([], "filename", "files"), ['filename/files'])
+        self.assertEqual(file.append_files(["file1"], "file2", "file"), ['file1', 'file2/file'])

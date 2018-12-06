f4a41f7862adcd93f297dfa7115fb6192eb72e1f
Abilash Rajasekaran <abilashr@IN-CBE-Abilash.local>
2018-12-06 14:42:51 +0530

    string concat using os path

--- app/utils/extract_value.py
+++ app/utils/extract_value.py
@@ -1,2 +1,5 @@
+import os
+
+
 def get_base_url_till_given_string(request, string):
-    return ''.join(request.base_url.split(string)[0])+string+'/'
+    return os.path.join(''.join([request.base_url.split(string)[0], string]), '')

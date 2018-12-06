ea642176d25e1251a068e53b08b9d042b6d84b8e
Elizabeth Chirayil <elizabeth.chirayil@thoughtworks.com>
2018-12-06 15:46:25 +0530

    test app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,12 +1,17 @@
-import unittest
+from unittest import TestCase
+from app import app
 from mock import patch
 
-from app import app
 
-
-class TestApp(unittest.TestCase):
+class TestApp(TestCase):
 
     @patch("app.app.render_home")
-    def test_render_home(self, render_home):
+    def test_home(self, render_home):
         render_home.return_value = 'success'
-        self.assertEqual(app.home(), "success")
+        self.assertEqual(app.home(), 'success')
+
+    @patch("app.app.render_category")
+    def test_category(self, render_category):
+        render_category.return_value = 'success'
+        file_name = 'dummy'
+        self.assertEqual(app.category(file_name), 'success')

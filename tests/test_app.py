d50847e20b4e081bcce88d5a78968494f8ec9603
Sreekesh S <in-sreekesh@In-Cbe-Sreekesh.local>
2018-12-06 15:30:58 +0530

    unit test for app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,7 +6,7 @@ from app import app
 
 class TestApp(unittest.TestCase):
 
-    @patch("app.app.home")
-    def test_render_home(self, r_home):
-        r_home.render_home.return_value = 'success'
+    @patch("app.app.render_home")
+    def test_render_home(self, render_home):
+        render_home.return_value = 'success'
         self.assertEqual(app.home(), "success")

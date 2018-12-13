1fa5741af5962be5609a930bd96dc99fe7fe9705
Sreekesh S <in-sreekesh@In-Cbe-Sreekesh.local>
2018-12-13 11:01:52 +0530

    removed hardcoded value from downloadble templates

--- app/core/category.py
+++ app/core/category.py
@@ -1,3 +1,6 @@
+from app.core.file_operations import get_json_file, get_json_file_path_from_data
+
+
 def get_category(path, json_data):
     for name in path:
         json_data = json_data['type'][name]
@@ -17,3 +20,18 @@ def get_category(path, json_data):
                 species_obj = {species['Name']: species for species in ckan_species_list_response['result']}
                 json_data['type'][key]['type'] = species_obj
     return json_data
+
+
+def get_species_from_path(category_type, path):
+    species_str = ''
+    for idx, name in enumerate(path):
+        if 'type' in category_type:
+            category_type = category_type['type'][name]
+        else:
+            species_str = path[idx:]
+    return species_str
+
+
+def get_resource_id(category_path):
+    category_type = get_json_file(get_json_file_path_from_data(category_path[0]))
+    return category_type['type'][category_path[0]]['resource_id']
--- app/core/services.py
+++ app/core/services.py
@@ -1,14 +1,14 @@
 from flask import render_template, request
+from ..utils.rest_client import get
+from ..utils.constants import environment_details, api, display_details
 from ..utils.extract_value import get_base_url_till_given_string
-from .category import get_category
+from .category import get_category, get_resource_id, get_species_from_path
 from .file_operations import get_visual_files, get_json_file_path_from_data, get_json_file, \
     home_page_category_data
-from ..utils.rest_client import get
-from ..utils.constants import api, display_details
 
 
 def render_home():
-    return render_template('home/home.html', json_data=home_page_category_data())
+    return render_template('home/home.html', ckan_url=environment_details['ckan'], json_data=home_page_category_data())
 
 
 def render_category(filename):
@@ -21,7 +21,7 @@ def render_category(filename):
     if species_str:
         return render_species_details(category_path)
 
-    return render_template('category/category.html', json_data=get_category(category_path, category_type),
+    return render_template('category/category.html', ckan_url=environment_details['ckan'], json_data=get_category(category_path, category_type),
                            fullpath=category_path,
                            js_files=get_visual_files(filename), base_url=base_url)
 
@@ -29,29 +29,14 @@ def render_category(filename):
 def render_species_details(path):
     species_name = path[len(path)-1]
     url = api['datastore_search']
-    resource_id = _get_resource_id(path)
+    resource_id = get_resource_id(path)
     query_params = {'resource_id': resource_id, 'filters': '{"species":"'+species_name+'"}', 'limit':'1'}
     response = get(url=url, queryparams=query_params)
     species_record = response['result']['records'][0]
     species_display_info = _get_filtered_details(species_record, display_details)
     species_display_info['is_species'] = 'true'
-    return render_template('species_detail/species_detail.html',species_name=species_name, json_data=species_display_info,
+    return render_template('species_detail/species_detail.html', ckan_url=environment_details['ckan'], species_name=species_name, json_data= species_display_info,
                            fullpath=path)
-
-
-def get_species_from_path(category_type, path):
-    species_str = ''
-    for idx, name in enumerate(path):
-        if 'type' in category_type:
-            category_type = category_type['type'][name]
-        else:
-            species_str = path[idx:]
-    return species_str
-
-
-def _get_resource_id(category_path):
-    category_type = get_json_file(get_json_file_path_from_data(category_path[0]))
-    return category_type['type'][category_path[0]]['resource_id']
 
 
 def _get_filtered_details(species_record,keys):

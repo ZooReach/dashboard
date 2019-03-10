b241680c33c1b3f5ca3c8da4da4b5979e23c4bf0
Abilash Rajasekaran <abilashr@IN-CBE-Abilash.local>
2019-03-10 19:49:27 +0530

    style page

--- tests/core/test_category.py
+++ tests/core/test_category.py
@@ -12,8 +12,8 @@ class CategoryTestCase(TestCase):
     @patch("app.core.category.get_category_data")
     @patch("app.core.category.get_species_data")
     def test_get_category(self,get_species_data, get_category_data):
-        get_species_data.return_value = [{"species": "tigerFish","genus":"animalia","type": {},"kingdom":"Kingdom","image": "images/placeholder.svg"}]
+        get_species_data.return_value = [{"species": "tigerFish","genus":"animalia","type": {},"kingdom":"Kingdom","image": "images/placeholder.png"}]
         get_category_data.return_value = []
         result = category.get_category('0', {})
-        self.assertEqual([{"name": "tigerFish","genus":"animalia","type": {},"kingdom":"Kingdom","image": "images/placeholder.svg"}], result)
+        self.assertEqual([{"name": "tigerFish","genus":"animalia","type": {},"kingdom":"Kingdom","image": "images/placeholder.png"}], result)
 

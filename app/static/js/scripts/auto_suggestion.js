73540c6a08dc56ab3569cf5212ef7b898f3ad4e7
kumawatanupriya <anupriyakumawat95@gmail.com>
2019-03-18 10:55:16 +0530

    Anupriya || Show visual chart in report page

--- app/static/js/scripts/auto_suggestion.js
+++ app/static/js/scripts/auto_suggestion.js
@@ -18,8 +18,51 @@ $( function() {
                 }
                 
 
-          })
+          });
       }
 
     }); 
+
+    $("#submit_search_data").click(function(){
+        var search_key  = $('#species-autocomplete').val();
+        var json_data = {}
+        var url = `/api/${search_key}`;
+        var res = d3.json(url);
+            d3.json(url).then( function (json_data) {   
+            var chart = c3.generate({
+                size: {
+                height: 400,
+                width: 400
+            },
+            bindto :"#visual_report",
+            data: {
+                json: json_data['data'],
+                    type : 'bar',
+                    keys: {
+                    x: 'category_level1',
+                    value: ['count']
+                }
+            },
+                axis: {
+                        x: {
+                            type: 'category'
+                        }
+                },
+            bar: {
+                width: {
+                    ratio: 0.15
+                }
+            }
+            });
+
+
+            setTimeout(function () {
+            chart.resize({height:400})
+            }, 1000);
+
+            }, function(error){
+                document.getElementById("visual_report").innerHTML = "No Report Found";
+            });
+
+    });
   });

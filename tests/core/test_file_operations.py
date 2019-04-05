8a700e8738e0320d2853f01c1079a27b9107c451
kumawatanupriya <anupriyakumawat95@gmail.com>
2019-04-05 12:48:12 +0530

    write test cases for file_operations

--- tests/core/test_file_operations.py
+++ tests/core/test_file_operations.py
@@ -1,6 +1,8 @@
 from unittest import TestCase
-from mock import patch
+from mock import patch, Mock
 from app.core import file_operations as file
+import mock
+
 
 
 class FileOperationTestCase(TestCase):
@@ -34,10 +36,43 @@ class FileOperationTestCase(TestCase):
         self.assertEqual(file.get_visual_files("fishes"),
                          ["js/visualization/dir1.js", "js/visualization/dir2.js", "js/visualization/dir3.js"])
 
+
+    @patch("app.core.file_operations.get_all_visual_data_files")
+    def test_get_all_visual_chart_files(self, get_all_visual_data_files):
+        get_all_visual_data_files.return_value = ['test', 'fishes', 'spiders', 'demo']
+        self.assertEqual(file.get_all_visual_chart_files(), 
+        ["js/visualization/test.js", "js/visualization/fishes.js", "js/visualization/spiders.js", "js/visualization/demo.js"])
+
+
+    def test_get_visual_file(self):
+        self.assertEqual(file.get_visual_file('fishes'), ["js/visualization/fishes.js"])
+
+
+    @patch("json.load")
+    @patch("app.core.file_operations.open", create=True)
+    def test_get_json_file(self, mock_open, json_load):
+        mock_open.side_effect = [
+            mock.mock_open(read_data='{"fishes":0, "demo":1}').return_value
+        ]
+        json_load.return_value = {"fishes":0, "demo":1}
+        self.assertEqual({"fishes":0, "demo":1}, file.get_json_file("fileA"))
+        
+
     @patch("json.load")
     def test_update_json(self, json_load):
         json_load.return_value = {"type": {"key": "value"}}
         self.assertEqual(file.update_json({}, "somefile"), {"key": "value"})
+
+
+    @patch("app.core.file_operations.update_json")
+    @patch("app.core.file_operations.open", create=True)
+    def test_update_json_from_file(self, mock_open, update_json):
+        update_json.return_value = {"key":"value"}
+        mock_open.side_effect = [
+            mock.mock_open(read_data={"type":{"key":"value"}}).return_value
+        ]
+        self.assertEqual(file.update_json_from_file({}, 'test', "x.txt"), {"key":"value"})
+
 
     @patch("app.core.file_operations.get_visual_data")
     def test_get_visual_map_from_db(self, get_visual_data):
